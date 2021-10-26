9337cb74a2a45ce44e58b6d331dac29069114233
Aryabobs <93197724+aryakings@users.noreply.github.com>
2021-10-26 20:06:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6289677763976', '6283808135990', '79224816516'] // Put your number here
+global.owner = ['6289677763976', '6289531660936', '62895316609363'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
